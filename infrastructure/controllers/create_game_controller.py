3f55ceefe78c98986d76b3ce71ad18e0d53202d5
Mauricio Pizarro <pizarromauricio031096@gmail.com>
2023-08-29 19:53:14 -0300

    fixing-turn-order-and-listeners (#9)
    * fixing-turn-order-and-listeners
    
    * fix test_crete_game

---
+++ infrastructure/controllers/create_game_controller.py
@@ -0,0 +1,22 @@
+from typing import List
+from pydantic import BaseModel
+from fastapi import APIRouter, HTTPException
+from application.create_game_service import CreateGameService
+
+
+router = APIRouter()
+create_game_service = CreateGameService()
+
+
+class Player(BaseModel):
+    name: str
+    user_id: str
+
+class CreateGameRequestModel(BaseModel):
+    game_id: str
+    players: List[Player]
+
+
+@router.post("/game/create")
+async def croupier_controller(request: CreateGameRequestModel):
+    create_game_service.create_game(request.players, request.game_id)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ time.sleep(10)
 from fastapi import FastAPI
 import infrastructure.injector # don't remove this dependecy
 from infrastructure.controllers import stand_controller, deal_card_controller, status_controller, croupier_controller, \
-    history_game_controller
+    history_game_controller, create_game_controller
 
 app = FastAPI()
 
@@ -12,3 +12,4 @@ app.include_router(status_controller.router)
 app.include_router(deal_card_controller.router)
 app.include_router(stand_controller.router)
 app.include_router(croupier_controller.router)
+app.include_router(create_game_controller.router)
