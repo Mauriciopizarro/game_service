db10868704ddee12cddff3e1ded09525cfaaa65f
Mauri <pizarromauricio031096@gmail.com>
2024-05-10 16:52:37 -0300

    adding sleep time in consumer

--- start_game_consumer.py
+++ start_game_consumer.py
@@ -1,8 +1,10 @@
 from threading import Thread
 from infrastructure.event_managers.consumers.create_game_consumer import CreateGameListener
+import time
 
 
 def start_consumer():
+    time.sleep(5)
     Thread(target=CreateGameListener).start()
 
 
