b9b1e9de61d3cce3ec6d3c60ba17e28300ab125c
Mauricio Pizarro <pizarromauricio031096@gmail.com>
2024-05-24 19:37:28 -0300

    improve-rabbitMq-messages-when-game-is-finished (#22)
    * improve-rabbitMq-messages-when-game-is-finished
    
    * adding MULTIPLY_BET_AMOUNT in env test

--- application/stand_service.py
+++ application/stand_service.py
@@ -2,6 +2,7 @@ from dependency_injector.wiring import Provide, inject
 from infrastructure.injector import Injector
 from domain.interfaces.game_repository import GameRepository
 from domain.interfaces.publisher import Publisher
+from config import settings
 
 class StandService:
 
@@ -15,15 +16,13 @@ class StandService:
     def stand(self, player_id, game_id):
         game = self.game_repository.get(game_id)
         game.stand_current_turn_player(player_id)
-
+        self.game_repository.update(game)
         if game.game_status == 'finished':
             for player in game.turn_order:
                 if player.name != "Croupier":
                     if player.status == 'winner':
                         message = {
                             "user_id": player.player_id,
-                            "amount": player.get_bet() * 2
+                            "amount": player.get_bet() * settings.MULTIPLY_BET_AMOUNT
                         }
                         self.publisher.send_message(message=message, topic="set_money_account")
-
-        self.game_repository.update(game)
