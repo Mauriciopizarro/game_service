bf001d3f7423d10fbc33844ee1e5f687b475391c
Mauri <pizarromauricio031096@gmail.com>
2024-12-19 18:12:33 -0300

    fix-test-deal-card

--- test/unit/application/test_deal_card.py
+++ test/unit/application/test_deal_card.py
@@ -40,24 +40,22 @@ def test_deal_card():
     assert game.turn_order[1].status == "waiting_turn"
     assert game.game_status == "started"
 
-"""
  
-# Player with 21 points should be winner, but that hasn't happened currently
+
  
-def test_deal_card_when_player_wins():
+def test_deal_card_when_player_win():
     game = create_mock_game(3) # With a 3 the player wins
     mock_game_repo = MockGameRepository(game)
-    deal_card_service = DealCardService(mock_game_repo)
+    deal_card_service = DealCardService(game_repository=mock_game_repo)
 
     deal_card_service.deal_card(player_id="63bcb88cfe7f81c8af8d9faf", game_id="63bcba244dbc3beb6fec0eb2")
 
     assert len(game.turn_order[0].cards) == 3
-    assert game.turn_order[0].cards[-1] == NumberCard(2)
+    assert game.turn_order[0].cards[-1] == NumberCard(3)
     assert game.turn_order[0].status == "playing"
     assert game.turn_order[1].status == "waiting_turn"
     assert game.game_status == "started"
 
-"""
 
 def test_deal_card_when_player_loose():
     game = create_mock_game(4) # With a 4 the player loose
