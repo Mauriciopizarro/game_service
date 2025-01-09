1feb43577ab6ad8f86438108c060370ca400d523
Mauri <pizarromauricio031096@gmail.com>
2025-01-08 22:20:11 -0300

    [MINOR] fix tests

--- test/unit/application/test_stand_player.py
+++ test/unit/application/test_stand_player.py
@@ -3,8 +3,13 @@ from application.stand_service import StandService
 from domain.card import LetterCard, NumberCard
 from domain.game import Game, IncorrectPlayerTurn
 from domain.player import Player, Croupier
+from test.utils.fake_publisher_interface import FakePublisherInterface
 from test.utils.mock_game_repository import MockGameRepository
 import pytest
+
+
+fake_publisher = FakePublisherInterface()
+
 
 def get_mocked_game(game_status: str, turn_position: int):
     game = Game(
@@ -43,7 +48,7 @@ def get_mocked_game(game_status: str, turn_position: int):
 def test_stand_player_game_finished():
     game = get_mocked_game(game_status="finished", turn_position=0) # Game finished raises GameFinishedError exception
     mock_game_repo = MockGameRepository(game)
-    stand_service = StandService(game_repository=mock_game_repo)
+    stand_service = StandService(game_repository=mock_game_repo, publisher=fake_publisher)
 
     with pytest.raises(GameFinishedError):
         stand_service.stand(player_id="63bcb88cfe7f81c8af8d9faf", game_id="63bcba244dbc3beb6fec0eb2")
@@ -51,7 +56,7 @@ def test_stand_player_game_finished():
 def test_stand_incorrect_player_turn():
     game = get_mocked_game(game_status="started", turn_position=1) # If turn_position not match with player_id raises IncorrectPlayerTurn exception
     mock_game_repo = MockGameRepository(game)
-    stand_service = StandService(game_repository=mock_game_repo)
+    stand_service = StandService(game_repository=mock_game_repo, publisher=fake_publisher)
 
     with pytest.raises(IncorrectPlayerTurn):
         stand_service.stand(player_id="63bcb88cfe7f81c8af8d9faf", game_id="63bcba244dbc3beb6fec0eb2")
